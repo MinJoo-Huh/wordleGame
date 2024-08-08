a1ed2ec216e81a5ddd3b043d57da786311d23f37
Minjoo Huh <huhminjoo@naver.com>
2024-08-09 02:30:28 +0900

    [워들] JavaScript 수정
    - 정답 애니메이션 추가
    - 창의 키보드를 누르면 입력

--- js/index.js
+++ js/index.js
@@ -3,27 +3,38 @@ const correct_answer = "APPLE";
 let attempts = 0; // 줄
 let index = 0; // 칸
 
+let keyboard_content; // 화면 키보드 문자
+
 let timer;
+
+let keyBlock;
 
 function appStart() {
   // Game over alert
   const displayGameover = () => {
+    // div 생성
     const div = document.createElement("div");
+    // div 내용 설정
     div.innerText = "게임이 종료되었습니다.";
+    // div CSS
     div.style =
-      "display:flex; justify-content:center; align-items:center; position:absolute; top:30vh; left:42vw; background-color:white; width:200px; height:100px";
+      "display:flex; justify-content:center; align-items:center; position:absolute; top:30vh; left:45vw; background-color:white; width:200px; height:100px ";
+    // div body 안에 생성
     document.body.appendChild(div);
   };
 
   // 다음 줄로 넘어가는 함수
   const nextLine = () => {
-    if (attempts === 6) return gameover();
+    // 아닐 경우, 다음줄로 넘어가고 다시 처음 index부터 시작
     attempts++;
     index = 0;
+    // attempts 6번이면 gameover
+    if (attempts === 6) return gameover();
   };
 
   // 백스페이스를 눌렀을 때 함수
   const handleBackSpace = () => {
+    //
     if (index > 0) {
       const preBlock = document.querySelector(
         `.board-block[data-index='${attempts}${index - 1}']`
@@ -52,7 +63,7 @@ function appStart() {
   const gameover = () => {
     window.removeEventListener("keybown", handleKeyboard);
     displayGameover();
-    clearInterval(timer);
+    clearInterval(timer); // timer 종료
   };
 
   // 한 줄이 완성되고 Enter 키가 눌려졌을 경우
@@ -73,6 +84,14 @@ function appStart() {
       if (letter === correct_letter) {
         correct_num++;
         block.style.background = "#6AAA64";
+
+        //block.style = "background-color='#6AAA64'; ";
+        let keyframes = [
+          { transform: "rotate(0) scale(1)" },
+          { transform: "rotateX(360deg) scale(1)" },
+        ];
+        let options = { duration: 1000, iterations: 1 };
+        block.animate(keyframes, options);
       } else if (correct_answer.includes(letter))
         block.style.background = "#C9B458";
       else block.style.background = "#787C7E";
@@ -90,6 +109,8 @@ function appStart() {
   const handleKeyboard = (event) => {
     const key = event.key.toUpperCase(); // 눌려진 키 대문자로 표현
     const keyCode = event.keyCode; // 눌러진 키의 코드를 가져온다.
+
+    console.log(key);
 
     // 블럭 정보를 가져온다.
     const thisBlock = document.querySelector(
@@ -109,8 +130,45 @@ function appStart() {
     }
   };
 
+  const window_keyboard = (event) => {
+    const key = event.target.dataset.key;
+    // 정보 가져오기 = event 정보 뒤져서 차례로 적기...ㅎ
+    console.log(key);
+
+    const keyBlock = document.querySelector(
+      `.keyboard-block[data-key='${key}']`
+    );
+
+    // 블럭 정보를 가져온다.
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+
+    console.log(keyBlock);
+
+    // BackSpace키를 눌렸을 경우
+    if (key === "BACK") handleBackSpace();
+    else if (index === 5) {
+      //console.log(event.key); // enter 오류 확인 => Enter
+      if (key === "ENTER") handleEneterKey();
+      else return;
+    } else {
+      console.log("key");
+      thisBlock.innerText = key;
+      index++;
+    }
+  };
+
   // timer 함수
   startTimer();
+
+  // keyboard-block(화면에 있는 키보드) div 가져오기
+  var keypad = document.getElementsByClassName("keyboard-block");
+
+  // keyboard-block click 이벤트 주기
+  Array.from(keypad).forEach(function (event) {
+    event.addEventListener("click", window_keyboard);
+  });
 
   // 키가 눌러지면 handleKeyboard 함수를 가져온다.
   window.addEventListener("keydown", handleKeyboard);
